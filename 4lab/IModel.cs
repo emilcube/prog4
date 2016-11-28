9524ba84788893f8ac50920a7b7930e8aba533f7
Timur Mukminov <leshy.lesovich@yandex.ru>
2016-11-28 08:45:59 +0500

    Добавьте файлы проекта.

---
+++ 4lab/IModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4lab
+{
+    interface IModel
+    {
+        void AddNode(string text);
+        void RemoveLastNode();
+        int Count { get; }
+
+
+
+    }
+}
---
+++ 4lab/MyNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4lab
+{
+    class MyNode
+    {
+        string text;
+        int x, y;
+        public MyNode(string text, int x, int y)
+            {
+        this.text=text;
+            this.x=x;
+            this.y=y;
+            }
+
+    }
+}
